eb1613029b671b71a119615d52fff097a50633f8
Vu Pham <pqv199x@hotmail.com>
2018-12-03 10:47:13 +0700

    testing 2

--- app/App.vue
+++ app/App.vue
@@ -61,7 +61,7 @@ export default {
         }
     },
     async updated () {
-        await this.checkNetworkAndLogin()
+        await this.checkNetworkAndLogin1()
     },
     created: async function () {
         let self = this
@@ -70,7 +70,7 @@ export default {
             if (!self.isReady && self.NetworkProvider === 'metamask') {
                 throw Error('Web3 is not properly detected. Have you installed MetaMask extension?')
             }
-            await self.checkNetworkAndLogin()
+            await self.checkNetworkAndLogin1()
         } catch (e) {
             console.log(e)
         }
@@ -101,7 +101,7 @@ export default {
         goPage: function (s) {
             this.$router.push({ path: `/candidate/${s}` })
         },
-        async checkNetworkAndLogin () {
+        async checkNetworkAndLogin1 () {
             setTimeout(async () => {
                 let account
                 try {
